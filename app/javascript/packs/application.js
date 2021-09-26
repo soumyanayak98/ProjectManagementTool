835f5dd1607803366850978ff828479dccb0cc58
soumyanayak98 <soumya.nayak@kreeti.com>
2021-09-26 20:46:12 +0530

    sidebar navigation for sections

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,4 +22,34 @@ $(document).on("turbolinks:load", function () {
 		let lastChild = task.lastElementChild;
 		lastChild.style.display = "none";
 	});
+
+	$("#currBtn").on("click", function () {
+		$("#currItr").hide();
+	});
+	$("#backBtn").on("click", function () {
+		$("#backlogs").hide();
+	});
+	$("#iceBtn").on("click", function () {
+		$("#icebox").hide();
+	});
+	$("#menu-icon").on("click", openNav);
+	$("#close-icon").on("click", closeNav);
+
+	$("#currOpen").on("click", function () {
+		$("#currItr").show();
+	});
+	$("#backOpen").on("click", function () {
+		$("#backlogs").show();
+	});
+	$("#iceOpen").on("click", function () {
+		$("#icebox").show();
+	});
 });
+
+function openNav() {
+	document.getElementById("mySidenav").style.width = "225px";
+}
+
+function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+}
