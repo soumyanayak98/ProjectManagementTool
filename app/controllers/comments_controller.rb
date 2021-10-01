152c727dccd64918dabd911d2ccc8cac48a2d383
soumyanayak98 <soumya.nayak@kreeti.com>
2021-10-01 14:52:37 +0530

    added some restriction on mailer

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,10 +6,12 @@ class CommentsController < ApplicationController
     @comment = @task.comments.build(comment_params)
     if @comment.save
       flash[:success]="Comment added successfully"
-      emails = @comment.task.users.map(&:email)
       owner_email = @comment.task.feature.project.user.email
-      @comment.task.users.each do |user|
-        CommentMailer.new_comment(@comment, user.email).deliver_now
+      emails = @comment.task.users.map(&:email)
+      if emails.size > 0
+        @comment.task.users.each do |user|
+          CommentMailer.new_comment(@comment, user.email).deliver_now
+        end
       end
       CommentMailer.new_comment(@comment, owner_email).deliver_now if !emails.include?(owner_email)
       redirect_to [@project, @feature, @task]
